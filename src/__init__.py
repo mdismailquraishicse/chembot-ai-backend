243775cbcb320bef96aea343e60ed4f3c8ada76c
Md Ismail Quraishi <mdismailquraishicse@gmail.com>
2026-05-15 14:37:04 +0530

    refactor: code optimization

--- src/__init__.py
+++ src/__init__.py
@@ -4,8 +4,6 @@ from fastapi.middleware.cors import CORSMiddleware
 
 os.environ["HUGGINGFACEHUB_API_TOKEN"] = os.getenv("HUGGINGFACEHUB_API_TOKEN")
 
-host = os.getenv("OLLAMA_HOST")
-model = os.getenv("OLLAMA_MODEL")
 huggingface_key = os.getenv("HUGGINGFACEHUB_API_TOKEN")
 use_local_model = os.getenv("USE_LOCAL_MODEL", "0")
 
